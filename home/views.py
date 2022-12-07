53869657f29901b42ec92a5fdd1b76c7d7f371eb
Rajabu Mrisho <rajabumrisho69@gmail.com>
2022-12-07 10:34:01 +0300

    ---

--- home/views.py
+++ home/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from .models import Blog
 # Create your views here.
+
+
+def blog(request, *args, **kwargs):
+    if request.method == 'POST':
+        print(request.POST)
+    return render(request, 'blog.html', {})
